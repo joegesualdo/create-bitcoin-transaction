34a808984d938132e9dfd9053e7122910bbfcc5c
Joe Gesualdo <joegesualdo@gmail.com>
2022-09-06 10:06:13 -0400

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 // Sources:
-// - http://www.righto.com/2014/02/bitcoins-hard-wa&y-using-raw-bitcoin.html
+// - http://www.righto.com/2014/02/bitcoins-hard-way-using-raw-bitcoin.html
 // - https://en.bitcoin.it/wiki/Transaction
 // - https://en.bitcoin.it/wiki/Protocol_documentation
 // - https://developer.bitcoin.org/reference/transactions.html#:~:text=Bitcoin%20transactions%20are%20broadcast%20between,part%20of%20the%20consensus%20rules.
 //
 // Can check work here: https://bc-2.jp/tools/txeditor2.html
+//
 //
 use hex_utilities::{
     convert_big_endian_hex_to_little_endian, convert_decimal_to_hexadecimal, decode_hex,
@@ -48,20 +49,26 @@ fn get_prev_transaction_output_index(index: u64) -> String {
     let hex_little_endian = convert_big_endian_hex_to_little_endian(&hex);
     hex_little_endian
 }
-fn get_input_script_length() -> String {
+fn get_input_script_length(input_script_hex: &str) -> String {
     // Hardcoded to the length of our hardcoded input scrip sig below
     // https://mempool.space/testnet/tx/2eabf6f8d63d25005866521c844449765e99e43948cb36dd6bbfad544a3d0f17
-    "19".to_string()
+    let input_script_hex_as_bytes = decode_hex(input_script_hex).unwrap();
+    let input_script_hex_bytes_count = input_script_hex_as_bytes.len();
+    let script_length_bytes_in_hex =
+        convert_decimal_to_hexadecimal(input_script_hex_bytes_count as u64, false, Some(1));
+    script_length_bytes_in_hex
 }
-fn get_input_script_sig() -> String {
+// Before signing, the script sig in the transaction should be equal to the script pub key hex from
+// the spending vout.
+fn get_input_script_sig(script_pub_key: &str) -> String {
     // Hardcoded
     // Before signing, the script sig should be set to the spending public script sig.
     // https://mempool.space/testnet/tx/2eabf6f8d63d25005866521c844449765e99e43948cb36dd6bbfad544a3d0f17
-    "76a914fee074ce1405ab8cfbacdd898354c736605dfd2b88ac".to_string()
+    script_pub_key.to_string()
 }
 // Hardcoding for now
-fn get_sequence() -> String {
-    "ffffffff".to_string()
+fn get_sequence(sequence: &str) -> String {
+    sequence.to_string()
 }
 
 fn get_output_count(output_count: u64) -> String {
@@ -87,11 +94,16 @@ fn get_output_amount(amount_in_btc: f64) -> String {
     let little_endian_amount_in_sats = convert_big_endian_hex_to_little_endian(&amount_in_sats_hex);
     little_endian_amount_in_sats
 }
-fn get_output_script_length() -> String {
-    "19".to_string()
+fn get_output_script_length(public_key_hash: &String) -> String {
+    let output_script_sig_hex = get_output_script_sig(public_key_hash.to_string());
+    let output_script_sig_hex_as_bytes = decode_hex(&output_script_sig_hex).unwrap();
+    let output_script_sig_hex_bytes_count = output_script_sig_hex_as_bytes.len();
+    let script_length_bytes_in_hex =
+        convert_decimal_to_hexadecimal(output_script_sig_hex_bytes_count as u64, false, Some(1));
+    script_length_bytes_in_hex
 }
-fn get_output_script_sig() -> String {
-    let public_key_hash_to_send_to = "f4d6e5314a4c7f40c779239d361bbdc8aedb7d11".to_string();
+fn get_output_script_sig(public_key_hash: String) -> String {
+    let public_key_hash_to_send_to = public_key_hash.to_string();
     format!("76a914{}88ac", public_key_hash_to_send_to)
 }
 fn get_lock_time() -> String {
@@ -101,43 +113,70 @@ fn get_lock_time() -> String {
 struct PayFrom {
     transaction: String,
     vout_index: u64,
+    script_pub_key_hex: String,
 }
-fn get_transaction(version: u8, pay_froms: Vec<PayFrom>) -> String {
+struct PayTo {
+    public_key_hash: String,
+    amount_in_btc: f64,
+}
+
+fn create_p2kh_transaction(version: u8, pay_froms: Vec<PayFrom>, pay_tos: Vec<PayTo>) -> String {
     let input_count = get_input_count(pay_froms.len() as u64);
     let mut input_part = String::new();
     input_part.push_str(&input_count);
     for pay_from in pay_froms {
         let part = format!(
-            "{}{}",
+            "{}{}{}{}{}",
             get_prev_transaction_hash(pay_from.transaction),
-            get_prev_transaction_output_index(pay_from.vout_index)
+            get_prev_transaction_output_index(pay_from.vout_index),
+            get_input_script_length(&pay_from.script_pub_key_hex),
+            get_input_script_sig(&pay_from.script_pub_key_hex),
+            get_sequence("ffffffff"),
         );
         input_part.push_str(&part);
     }
+    let output_count = get_output_count(pay_tos.len() as u64);
+    let mut output_part = String::new();
+    output_part.push_str(&output_count);
+    for pay_to in pay_tos {
+        let part = format!(
+            "{}{}{}",
+            get_output_amount(pay_to.amount_in_btc),
+            get_output_script_length(&pay_to.public_key_hash),
+            get_output_script_sig(pay_to.public_key_hash),
+        );
+        output_part.push_str(&part);
+    }
+
     let transaction = format!(
-        "{}{}{}{}{}{}{}{}{}{}",
+        "{}{}{}{}",
         get_version(version),
         input_part,
-        get_input_script_length(),
-        get_input_script_sig(),
-        get_sequence(),
-        get_output_count(1),
-        get_output_amount(0.00005),
-        get_output_script_length(),
-        get_output_script_sig(),
+        output_part,
         get_lock_time(),
     );
     transaction
 }
 
 fn main() {
-    let transaction = get_transaction(
+    let transaction = create_p2kh_transaction(
         1,
         vec![PayFrom {
-            transaction: "2eabf6f8d63d25005866521c844449765e99e43948cb36dd6bbfad544a3d0f17"
+            transaction: "a867c9b65d4262ef4fc9d14c236af1a9a3ad274fb808f27cdffe5e4fdaf2c926"
                 .to_string(),
-            vout_index: 1,
+            vout_index: 0,
+            script_pub_key_hex: "76a9147576478909598ec9a79aab2ce9cad22eef7cad6888ac".to_string(),
         }],
+        vec![
+            PayTo {
+                public_key_hash: "75a530d4fd70f55e5bc732cd3e20111f5ccbe781".to_string(),
+                amount_in_btc: 0.00005,
+            },
+            PayTo {
+                public_key_hash: "34010424a0ad16c98b6cfb3e551217cf5f0c250a".to_string(),
+                amount_in_btc: 0.00004,
+            },
+        ],
     );
     println!("{}", transaction)
 }
